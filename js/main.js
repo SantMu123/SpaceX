010cccc44a3b469bb98e34edf2ebddcf3a05b9a7
SantMu123 <126543033+SantMu123@users.noreply.github.com>
2024-05-08 00:05:31 +0000

    feat: :construction: Enlace de Rockets con el footer

--- js/main.js
+++ js/main.js
@@ -2,6 +2,21 @@ import {
     paginationRockets
 } from "./modulesComponents/pagination.js"
 
-let paginacion = document.querySelector("#paginacion")
-paginacion.innerHTML = ""
-paginacion.append(await paginationRockets())
+let footerSelect = async(e, id)=>{
+    e.preventDefault();
+    let li = document.querySelectorAll(".footer ul li")
+    for(let val of li){
+        let [a] = val.children
+        a.classList.remove('select');
+    }
+    let [a] = id.children
+    a.classList.add('select');   
+}
+
+let rocket = document.querySelector("#rocket")
+rocket.addEventListener("click", async(e)=>{
+    await footerSelect(e, rocket)
+    let paginacion = document.querySelector("#paginacion");
+    paginacion.innerHTML = ""
+    paginacion.append(await paginationRockets())
+} )
